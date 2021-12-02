2a42730a97355ddaa69d2b394face74b84b2c120
jorge <jorge.arranz@gmail.com>
2021-12-03 00:16:51 +0100

    rootReducer created

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,11 +1,8 @@
-import { types } from "../types/types"
-import { combineReducers } from "redux"
-import { uiReducers } from "./uiReducers"
-
-
-
+import { types } from "../types/types";
+import { combineReducers } from "redux";
+import { uiReducers } from "./uiReducers";
 
 export const rootReducer = combineReducers({
-    ui:uiReducers,
-    //TODO: agregar reducers
-})
+  ui: uiReducers,
+  //TODO: agregar reducers
+});
